aeb6368504568e234899a3fded1346f5ccb7d249
Christian Arvin <naitsirhc.uriel@gmail.com>
2018-06-13 13:10:08 +0800

    updated important uFS Template
    updated uFS Template from experimental_beta 2554dd1

--- ufs/core/init.sh
+++ ufs/core/init.sh
@@ -21,27 +21,25 @@ TMH5="TEST"
 ui_print(){
 	echo -n -e "ui_print $1\n" >> /proc/self/fd/$OUTFD
 	echo -n -e "ui_print\n"    >> /proc/self/fd/$OUTFD
-	echo "$1" >&5
-	cold_log "$1"
+	TMP_LOG="${TMP_LOG}"$'\n'"$(date "+%H:%M:%S") $1"
 }
 
 cold_log() {
-	LogTime=`date "+%H:%M:%S"`
-	TMP_LOG="${TMP_LOG}"$'\n'"$LogTime $1"
+	TMP_LOG="${TMP_LOG}"$'\n'"$(date "+%H:%M:%S") $1"
 	printf "$1\n"
 }
 
 flush_log(){
 	cold_log "I: INIT.SH: Flushing TMP_LOG"
+	cold_log "I: INIT.SH: Flushing DONE!!"
 	uFS_TL=/sdcard/logs/devlogs/$uFS_name"_flush.log"
 	echo $'\n\n\n'"FLUSH LOG $(date)"$'\n' >> $uFS_TL
 	echo "$TMP_LOG"                        >> $uFS_TL
 	echo $'\n'"DONE..."$'\n\n'             >> $uFS_TL
-	cold_log "I: INIT.SH: Flushing DONE!!"
 }
 
 cold_log=cold_log
-[ -z "$TMP_LOG" ] && TMP_LOG="TMP_LOG INIT BY INSTALLER.SH"
+[ -z "$TMP_LOG" ] && TMP_LOG="TMP_LOG INIT BY INIT.SH"
 
 
 # INIT
@@ -49,7 +47,7 @@ cold_log=cold_log
 #
 
 # PRINT ESSENTIAL DEF
-$cold_log "D: INSTALLER.SH: listing update-binary variables
+$cold_log "D: INIT.SH: listing update-binary variables
 COREDIR   -> $COREDIR
 BINARIES  -> $BINARIES
 LIBS      -> $LIBS
@@ -85,23 +83,21 @@ ERR=0
 #
 
 ASLIB=$LIBS/aslib
-$cold_log "I: INSTALLER.SH: LOADING $ASLIB"
-(eval . $ASLIB) && \
+$cold_log "I: INIT.SH: LOADING $ASLIB"
+(eval . $ASLIB 1>/dev/null) && \
 . $ASLIB || {
 	ui_print "E: ERROR $E_ANL: aslib is not loadable!!"
 	abort $E_ANL
 }
 
-ui_print "$E_ANL"
-
 # LOAD UFS CONFIGS
 ER_CODE=$COREDIR/config/er_code
 UFSCONFIG=$COREDIR/config/ufsconfig
 for config in $ER_CODE $UFSCONFIG; do
-	$cold_log "I: INSTALLER.SH: LOADING $config"
-	(eval . $config) && \
+	$cold_log "I: INIT.SH: LOADING $config"
+	(eval . $config 1>/dev/null) && \
 	. $config  || \
-	$cold_log "E: INSTALLER.SH: INTEGRITY UNABLE TO LOAD"
+	$cold_log "E: INIT.SH: INTEGRITY UNABLE TO LOAD"
 done
 
 # LOAD USER CONFIGS
@@ -109,13 +105,12 @@ USERCONFIG=$COREDIR/install/config
 USER_INSTALLSH=$COREDIR/install/install.sh 
 ( unzip -o "$ZIP" "install/*" -d "$COREDIR" ) && \
 for config in $USERCONFIG $USER_INSTALLSH; do
-	$cold_log "I: INSTALLER.SH: LOADING $config"
-	(eval . $config) && \
+	$cold_log "I: INIT.SH: LOADING $config"
+	(eval . $config 1>/dev/null) && \
 	. $config  || \
-	$cold_log "E: INSTALLER.SH: INTEGRITY UNABLE TO LOAD"
+	$cold_log "E: INIT.SH: INTEGRITY UNABLE TO LOAD"
 done || \
-cold_log "W: INSTALLER.SH: UNABLE TO UNZIP USER CONFIGS"
-
+cold_log "W: INIT.SH: UNABLE TO UNZIP USER CONFIGS"
 
 # SETUP ASLIB
 alLSet type 	$aslib_log_type        # FLASH TYPE
@@ -128,16 +123,15 @@ loc_of_config=$UFSCONFIG
 loc_of_aslib=$ASLIB
 def_config_check || ui_print "W: Misconfigs Detetected! check logs"
 
-
-# LOAD INSTALLER.SH
+# LOAD INIT.SH
 # ___________________________________________________________________________________________________________ <- 110 char
 #
 
-# TURN-OVER TO USER INSTALLER.SH: IF PRESENT IN INSTALL FOLDER
+# TURN-OVER TO USER QUALIFIED CANDIDATE INSTALLER.SH
 USER_INSTALLERSH=$COREDIR/install/installer.sh
 CORE_INSTALLERSH=$COREDIR/core/installer.sh
 [ -e $USER_INSTALLERSH ] && {
-	$cold_log "I: INIT.SH: DETECTED USER INSTALLER.SH, HANDING OVER.."
+	$cold_log "I: INIT.SH: DETECTED USER INIT.SH, HANDING OVER.."
 	(
 		. $USER_INSTALLERSH "$@"
 		ec="$?"
@@ -155,4 +149,5 @@ CORE_INSTALLERSH=$COREDIR/core/installer.sh
 	) || INSTL_ERR=$? && INSTL_ERR=0
 }
 
+# EXIT WITH ERRCODE FROM INSTALLER.SH
 exit "$INSTL_ERR"
